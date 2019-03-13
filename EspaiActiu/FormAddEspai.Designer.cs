df71c09e360c9c78e04c7fb979bc2e0ed9dba6ed
Daniel <dalegalvan15@gmail.com>
2019-03-13 22:32:11 +0100

    espai labels

--- EspaiActiu/FormAddEspai.Designer.cs
+++ EspaiActiu/FormAddEspai.Designer.cs
@@ -28,6 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lHorari = new System.Windows.Forms.Label();
+            this.lTel = new System.Windows.Forms.Label();
+            this.lMesInfo = new System.Windows.Forms.Label();
+            this.lDireccio = new System.Windows.Forms.Label();
+            this.lNom = new System.Windows.Forms.Label();
             this.dtvHoraris = new System.Windows.Forms.DataGridView();
             this.lNouEspai = new System.Windows.Forms.Label();
             this.bAceptar = new System.Windows.Forms.Button();
@@ -39,87 +44,137 @@
             ((System.ComponentModel.ISupportInitialize)(this.dtvHoraris)).BeginInit();
             this.SuspendLayout();
             // 
+            // lHorari
+            // 
+            this.lHorari.AutoSize = true;
+            this.lHorari.Location = new System.Drawing.Point(76, 380);
+            this.lHorari.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lHorari.Name = "lHorari";
+            this.lHorari.Size = new System.Drawing.Size(64, 20);
+            this.lHorari.TabIndex = 70;
+            this.lHorari.Text = "Horaris:";
+            // 
+            // lTel
+            // 
+            this.lTel.AutoSize = true;
+            this.lTel.Location = new System.Drawing.Point(76, 291);
+            this.lTel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lTel.Name = "lTel";
+            this.lTel.Size = new System.Drawing.Size(150, 20);
+            this.lTel.TabIndex = 69;
+            this.lTel.Text = "Telèfon de contacte";
+            // 
+            // lMesInfo
+            // 
+            this.lMesInfo.AutoSize = true;
+            this.lMesInfo.Location = new System.Drawing.Point(76, 215);
+            this.lMesInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lMesInfo.Name = "lMesInfo";
+            this.lMesInfo.Size = new System.Drawing.Size(69, 20);
+            this.lMesInfo.TabIndex = 68;
+            this.lMesInfo.Text = "Més info";
+            // 
+            // lDireccio
+            // 
+            this.lDireccio.AutoSize = true;
+            this.lDireccio.Location = new System.Drawing.Point(76, 144);
+            this.lDireccio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lDireccio.Name = "lDireccio";
+            this.lDireccio.Size = new System.Drawing.Size(66, 20);
+            this.lDireccio.TabIndex = 67;
+            this.lDireccio.Text = "Direcció";
+            // 
+            // lNom
+            // 
+            this.lNom.AutoSize = true;
+            this.lNom.Location = new System.Drawing.Point(76, 69);
+            this.lNom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lNom.Name = "lNom";
+            this.lNom.Size = new System.Drawing.Size(42, 20);
+            this.lNom.TabIndex = 66;
+            this.lNom.Text = "Nom";
+            // 
             // dtvHoraris
             // 
             this.dtvHoraris.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtvHoraris.Location = new System.Drawing.Point(88, 379);
-            this.dtvHoraris.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.dtvHoraris.Location = new System.Drawing.Point(76, 404);
+            this.dtvHoraris.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dtvHoraris.Name = "dtvHoraris";
-            this.dtvHoraris.Size = new System.Drawing.Size(390, 214);
-            this.dtvHoraris.TabIndex = 52;
+            this.dtvHoraris.Size = new System.Drawing.Size(390, 209);
+            this.dtvHoraris.TabIndex = 65;
             // 
             // lNouEspai
             // 
             this.lNouEspai.AutoSize = true;
-            this.lNouEspai.Location = new System.Drawing.Point(227, 16);
-            this.lNouEspai.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lNouEspai.Location = new System.Drawing.Point(217, 21);
+            this.lNouEspai.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lNouEspai.Name = "lNouEspai";
             this.lNouEspai.Size = new System.Drawing.Size(80, 20);
-            this.lNouEspai.TabIndex = 51;
+            this.lNouEspai.TabIndex = 64;
             this.lNouEspai.Text = "Nou espai";
             // 
             // bAceptar
             // 
-            this.bAceptar.Location = new System.Drawing.Point(88, 639);
-            this.bAceptar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.bAceptar.Location = new System.Drawing.Point(76, 644);
+            this.bAceptar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bAceptar.Name = "bAceptar";
-            this.bAceptar.Size = new System.Drawing.Size(113, 49);
-            this.bAceptar.TabIndex = 50;
+            this.bAceptar.Size = new System.Drawing.Size(112, 49);
+            this.bAceptar.TabIndex = 63;
             this.bAceptar.Text = "Aceptar";
             this.bAceptar.UseVisualStyleBackColor = true;
             // 
             // bSortir
             // 
-            this.bSortir.Location = new System.Drawing.Point(365, 639);
-            this.bSortir.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.bSortir.Location = new System.Drawing.Point(353, 644);
+            this.bSortir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bSortir.Name = "bSortir";
-            this.bSortir.Size = new System.Drawing.Size(113, 49);
-            this.bSortir.TabIndex = 49;
+            this.bSortir.Size = new System.Drawing.Size(112, 49);
+            this.bSortir.TabIndex = 62;
             this.bSortir.Text = "Sortir";
             this.bSortir.UseVisualStyleBackColor = true;
-            this.bSortir.Click += new System.EventHandler(this.bSortir_Click);
             // 
             // tbTelefonEspai
             // 
-            this.tbTelefonEspai.Location = new System.Drawing.Point(88, 314);
-            this.tbTelefonEspai.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tbTelefonEspai.Location = new System.Drawing.Point(76, 315);
+            this.tbTelefonEspai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbTelefonEspai.Name = "tbTelefonEspai";
             this.tbTelefonEspai.Size = new System.Drawing.Size(388, 26);
-            this.tbTelefonEspai.TabIndex = 48;
-            this.tbTelefonEspai.Text = "Telèfon de contacte";
+            this.tbTelefonEspai.TabIndex = 61;
             // 
             // tbInfo
             // 
-            this.tbInfo.Location = new System.Drawing.Point(88, 237);
-            this.tbInfo.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tbInfo.Location = new System.Drawing.Point(76, 240);
+            this.tbInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbInfo.Name = "tbInfo";
             this.tbInfo.Size = new System.Drawing.Size(388, 26);
-            this.tbInfo.TabIndex = 47;
-            this.tbInfo.Text = "Mes info";
+            this.tbInfo.TabIndex = 60;
             // 
             // tbDireccio
             // 
-            this.tbDireccio.Location = new System.Drawing.Point(88, 165);
-            this.tbDireccio.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tbDireccio.Location = new System.Drawing.Point(76, 169);
+            this.tbDireccio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbDireccio.Name = "tbDireccio";
             this.tbDireccio.Size = new System.Drawing.Size(388, 26);
-            this.tbDireccio.TabIndex = 46;
-            this.tbDireccio.Text = "Direcció";
+            this.tbDireccio.TabIndex = 59;
             // 
             // tbNom
             // 
-            this.tbNom.Location = new System.Drawing.Point(88, 92);
-            this.tbNom.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tbNom.Location = new System.Drawing.Point(76, 98);
+            this.tbNom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbNom.Name = "tbNom";
             this.tbNom.Size = new System.Drawing.Size(388, 26);
-            this.tbNom.TabIndex = 45;
-            this.tbNom.Text = "Nom";
+            this.tbNom.TabIndex = 58;
             // 
             // FormAddEspai
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 714);
+            this.Controls.Add(this.lHorari);
+            this.Controls.Add(this.lTel);
+            this.Controls.Add(this.lMesInfo);
+            this.Controls.Add(this.lDireccio);
+            this.Controls.Add(this.lNom);
             this.Controls.Add(this.dtvHoraris);
             this.Controls.Add(this.lNouEspai);
             this.Controls.Add(this.bAceptar);
@@ -128,7 +183,7 @@
             this.Controls.Add(this.tbInfo);
             this.Controls.Add(this.tbDireccio);
             this.Controls.Add(this.tbNom);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormAddEspai";
             this.Text = "FormAddEspai";
             ((System.ComponentModel.ISupportInitialize)(this.dtvHoraris)).EndInit();
@@ -139,6 +194,11 @@
 
         #endregion
 
+        private System.Windows.Forms.Label lHorari;
+        private System.Windows.Forms.Label lTel;
+        private System.Windows.Forms.Label lMesInfo;
+        private System.Windows.Forms.Label lDireccio;
+        private System.Windows.Forms.Label lNom;
         private System.Windows.Forms.DataGridView dtvHoraris;
         private System.Windows.Forms.Label lNouEspai;
         private System.Windows.Forms.Button bAceptar;
